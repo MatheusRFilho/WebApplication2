d7d2a4852e8f9b1abd5f9b149bfc037fb9d2e366
MatheusRFilho <matheusrodrigofilho@outlook.com>
2020-10-07 20:01:30 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Controllers/TesteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public string nome()
+        {
+            return "Luis Fellipe";
+        }
+        public ActionResult Pagina()
+        {
+
+            return View();
+        }
+    }
+}
